d67c4ebeb3d7eaace6d6bfefaa49334eb16de554
Teddy Kitchen <teddykitchen@teddys-mbp-2.att.net>
2014-08-28 15:01:35 -0400

    about to make some changes

---
+++ CosmicBlast/CBEnemy.h
@@ -0,0 +1,13 @@
+//
+//  CBEnemy.h
+//  CosmicBlast
+//
+//  Created by Teddy Kitchen on 8/23/14.
+//  Copyright (c) 2014 Teddy Kitchen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface CBEnemy : SKSpriteNode
+
+@end
---
+++ CosmicBlast/CBEnemy.m
@@ -0,0 +1,13 @@
+//
+//  CBEnemy.m
+//  CosmicBlast
+//
+//  Created by Teddy Kitchen on 8/23/14.
+//  Copyright (c) 2014 Teddy Kitchen. All rights reserved.
+//
+
+#import "CBEnemy.h"
+
+@implementation CBEnemy
+
+@end
