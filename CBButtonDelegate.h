71083465cd9a0008a8eb2a555024570acdb7513d
Teddy Kitchen <kitchenteddy@gmail.com>
2016-06-03 19:20:21 -0400

    added restart button and added listener to allow the scene to tell the viewController to switch scenes

---
+++ CBButtonDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CBButtonDelegate.h
+//  CosmicBlast
+//
+//  Created by Teddy Kitchen on 6/2/16.
+//  Copyright © 2016 Teddy Kitchen. All rights reserved.
+//
+
+#ifndef CBButtonDelegate_h
+#define CBButtonDelegate_h
+
+
+#endif /* CBButtonDelegate_h */
---
+++ CosmicBlast/CBGameSceneDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CBGameSceneDelegate.h
+//  CosmicBlast
+//
+//  Created by Teddy Kitchen on 6/3/16.
+//  Copyright © 2016 Teddy Kitchen. All rights reserved.
+//
+
+#ifndef CBGameSceneDelegate_h
+#define CBGameSceneDelegate_h
+
+
+#endif /* CBGameSceneDelegate_h */
