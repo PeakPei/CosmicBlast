d7f10570d0c093a1fc01231bd12acbf501ae2ed1
Teddy Kitchen <kitchenteddy@gmail.com>
2016-12-06 21:03:51 -0500

    added JSON reading!  our database initialization now gets data out of levelJSON.  Still need to customize more things

--- CosmicBlast/LevelData.swift
+++ CosmicBlast/LevelData.swift
@@ -41,8 +41,11 @@ class LevelData:NSObject {
         let bundle = Bundle.main
         if let resourceUrl = bundle.url(forResource: "LevelsJSON", withExtension: "txt") {
             do {
-                let fileContents = try String(contentsOf: resourceUrl)
+                let fileContents = try Data(contentsOf: resourceUrl)
                 print("contents of file = \(fileContents)")
+                let json = JSON(data: fileContents)
+                print(json.count)
+                print(json["levels"].count)//[0]["number"])
             } catch {
                 print("error")
             }
@@ -51,29 +54,66 @@ class LevelData:NSObject {
     
     
     
+    
     func populateLevelsDictionary() {
-        //Need to adjust when we add more levels
-        //var levels = [LevelValues]()
-        UserDefaults().set(3, forKey: "availableLevels")
         
-        var pointArray1 = [NSValue]()
-        pointArray1.append(NSValue.init(cgPoint: CGPoint(x: 0,y: 0)));
-        let level1 = LevelValues(width: 1.0, height: 1.0, facLocs:pointArray1, levNum: 1.0)
-        levels.append(level1)
-        
-        var pointArray2 = [NSValue]()
-        pointArray2.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 150)));
-        pointArray2.append(NSValue.init(cgPoint: CGPoint(x: -200,y: -150)));
-        let level2 = LevelValues(width: 1.5, height: 1.0, facLocs:pointArray2, levNum: 2.0)
-        levels.append(level2)
-        
-        var pointArray3 = [NSValue]()
-        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 190)));
-        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 150)));
-        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 20,y: 190)));
-        let level3 = LevelValues(width: 1.0, height: 1.5, facLocs:pointArray3, levNum: 3.0)
-        levels.append(level3)
+        let bundle = Bundle.main
+        if let resourceUrl = bundle.url(forResource: "LevelsJSON", withExtension: "txt") {
+            do {
+                let fileContents = try Data(contentsOf: resourceUrl)
+                let json = JSON(data: fileContents)
+                UserDefaults().set(json["levels"].count, forKey: "availableLevels")
+            
+                for (_,level):(String, JSON)in json["levels"] {
+                    var pointArray = [NSValue]()
+                    let levelNumber = NSNumber(integerLiteral: level["number"].intValue)
+                   
+                    for (_,factory):(String, JSON) in level["factories"] {
+                        let facX = factory["x"].intValue
+                        let facY = factory["y"].intValue
+                        pointArray.append(NSValue.init(cgPoint: CGPoint(x: facX, y: facY)))
+                        //Do something you want
+                    }
+                    levels.append(LevelValues(width: 1.5, height: 1.5, facLocs: pointArray, levNum: levelNumber))
+//                    for (_,factory) in jsonLevel["factories"] {
+//                        pointArray.append(NSValue.init(cgPoint: CGPoint(x: factory["x"], y: factory["y"])))
+//                    }
+//                    let level = LevelValues(width: 1.0, height: 1.0, facLocs:pointArray1, levNum: 1.0)
+                }
+            } catch {
+                print("error finding levesJSON file in bundle")
+            }
+        }
     }
+    
+    
+    
+    
+    
+//    func populateLevelsDictionary() {
+//        
+//        //Need to adjust when we add more levels
+//        //var levels = [LevelValues]()
+//        UserDefaults().set(3, forKey: "availableLevels")
+//        
+//        var pointArray1 = [NSValue]()
+//        pointArray1.append(NSValue.init(cgPoint: CGPoint(x: 0,y: 0)));
+//        let level1 = LevelValues(width: 1.0, height: 1.0, facLocs:pointArray1, levNum: 1.0)
+//        levels.append(level1)
+//        
+//        var pointArray2 = [NSValue]()
+//        pointArray2.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 150)));
+//        pointArray2.append(NSValue.init(cgPoint: CGPoint(x: -200,y: -150)));
+//        let level2 = LevelValues(width: 1.5, height: 1.0, facLocs:pointArray2, levNum: 2.0)
+//        levels.append(level2)
+//        
+//        var pointArray3 = [NSValue]()
+//        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 190)));
+//        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 200,y: 150)));
+//        pointArray3.append(NSValue.init(cgPoint: CGPoint(x: 20,y: 190)));
+//        let level3 = LevelValues(width: 1.0, height: 1.5, facLocs:pointArray3, levNum: 3.0)
+//        levels.append(level3)
+//    }
     
     
     
